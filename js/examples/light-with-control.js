bb04e83b342d02b268a8a6b56d437210ed678029
Aurimas Cincikas <aurcin@gmail.com>
2023-04-26 17:55:55 +0300

    controlable light example

---
+++ js/examples/light-with-control.js
@@ -0,0 +1,49 @@
+const MAX_SIZE = 1500;
+
+const anim = gsap.to('.box', {
+  scale: MAX_SIZE,
+  duration: 8,
+  ease: 'linear',
+
+  stagger: {
+    each: 2,
+    repeat: -1,
+
+    onRepeat: updateZIndex,
+  },
+});
+
+function updateZIndex() {
+  let boxNumber = this._targets[0].dataset.number;
+  if (boxNumber === '4') {
+    gsap.set('.box', {
+      css: {
+        zIndex: 0,
+      },
+    });
+  } else {
+    gsap.set(this._targets, {
+      css: {
+        zIndex: 1,
+      },
+    });
+  }
+}
+
+const switcher = document.querySelector('#switch');
+let isLightOn;
+
+switcher.addEventListener('click', onSwitch);
+
+function onSwitch() {
+  gsap.fromTo(
+    switcher,
+    { scaleY: 1 },
+    { scaleY: 3, duration: 0.3, yoyo: true, repeat: 1 }
+  );
+  if (anim.isActive()) {
+    anim.seek(0).pause();
+  } else {
+    anim.resume();
+  }
+}
