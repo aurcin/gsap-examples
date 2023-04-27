bacbf2d9d7e5330d7f9b911d91787f47bf796bde
Aurimas Cincikas <aurcin@gmail.com>
2023-04-27 19:34:10 +0300

    very basic scroll trigger example

---
+++ plugins/scroll triger/basic example/main.js
@@ -0,0 +1,36 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.cube[data-cube="1"]', {
+  scrollTrigger: '.cube[data-cube="1"]',
+  x: 400,
+  rotation: 360,
+  duration: 3,
+});
+
+gsap.to('.cube[data-cube="11"]', {
+  scrollTrigger: '.cube[data-cube="11"]',
+  x: 400,
+  rotation: 360,
+  duration: 3,
+});
+
+gsap.to('.cube[data-cube="12"]', {
+  scrollTrigger: '.cube[data-cube="12"]',
+  x: 400,
+  rotation: 360,
+  duration: 3,
+});
+
+gsap.to('.cube[data-cube="13"]', {
+  scrollTrigger: '.cube[data-cube="13"]',
+  x: 400,
+  rotation: 360,
+  duration: 3,
+});
+
+gsap.to('.cube[data-cube="14"]', {
+  scrollTrigger: '.cube[data-cube="14"]',
+  x: 400,
+  rotation: 360,
+  duration: 3,
+});
