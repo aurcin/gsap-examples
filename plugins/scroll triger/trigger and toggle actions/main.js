0744ff16c03f7e57f54a6119ebab7edf24b74662
Aurimas Cincikas <aurcin@gmail.com>
2023-04-27 20:25:53 +0300

    toggle actions

---
+++ plugins/scroll triger/trigger and toggle actions/main.js
@@ -0,0 +1,51 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.cube[data-cube="1"]', {
+  scrollTrigger: {
+    trigger: '.cube[data-cube="1"]',
+    toggleActions: 'restart pause reverse pause',
+  },
+  x: 400,
+  rotation: 360,
+  duration: 3,
+});
+
+gsap.to('.cube[data-cube="11"]', {
+  scrollTrigger: {
+    trigger: '.cube[data-cube="11"]',
+    toggleActions: 'restart pause reverse pause',
+  },
+  x: 400,
+  rotation: 360,
+  duration: 3,
+});
+
+gsap.to('.cube[data-cube="12"]', {
+  scrollTrigger: {
+    trigger: '.cube[data-cube="12"]',
+    toggleActions: 'restart pause reverse pause',
+  },
+  x: 400,
+  rotation: 360,
+  duration: 3,
+});
+
+gsap.to('.cube[data-cube="13"]', {
+  scrollTrigger: {
+    trigger: '.cube[data-cube="13"]',
+    toggleActions: 'restart pause reverse pause',
+  },
+  x: 400,
+  rotation: 360,
+  duration: 3,
+});
+
+gsap.to('.cube[data-cube="14"]', {
+  scrollTrigger: {
+    trigger: '.cube[data-cube="14"]',
+    toggleActions: 'restart pause reverse pause',
+  },
+  x: 400,
+  rotation: 360,
+  duration: 3,
+});
