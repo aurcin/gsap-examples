3324ddbebd6a2384ecc1b9e45bd9d2f888c293c3
Aurimas Cincikas <aurcin@gmail.com>
2023-04-30 19:29:10 +0300

    pinning

---
+++ plugins/scroll triger/pinning/main.js
@@ -0,0 +1,15 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.cube[data-cube="11"]', {
+  scrollTrigger: {
+    trigger: '.cube[data-cube="11"]',
+    start: 'top 80%',
+    end: 'bottom 20%',
+    scrub: true,
+    pin: '.cube[data-cube="11"]',
+    markers: true,
+  },
+  x: 400,
+  rotation: 360,
+  duration: 3,
+});
