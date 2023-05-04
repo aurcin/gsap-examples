601cd9a4f9d234180044f284ae88bd01efebf06f
Aurimas Cincikas <aurcin@gmail.com>
2023-05-04 17:13:50 +0300

    initial horizontal layout container

---
+++ examples/layouts/horizontal container/main.js
@@ -0,0 +1,22 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to('.arrow', {
+//   y: 10,
+//   duration: 2,
+//   ease: 'none',
+//   repeat: -1,
+//   yoyo: true,
+// });
+
+// const sections = gsap.utils.toArray('.content-container>.container');
+// const scrollTween = gsap.to(sections, {
+//   xPercent: -100 * (sections.length - 1),
+//   ease: 'none', // <-- IMPORTANT!
+//   scrollTrigger: {
+//     trigger: '.content-container',
+//     pin: true,
+//     scrub: 0.1,
+//     //snap: 1 / (sections.length - 1),
+//     end: '+=3000',
+//   },
+// });
