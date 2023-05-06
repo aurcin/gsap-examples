ad197f4b5f914b82eef7be4ef0aecbf3b1484dbb
Aurimas Cincikas <aurcin@gmail.com>
2023-05-06 19:26:14 +0300

    horizontal layout example done

--- examples/layouts/horizontal container/main.js
+++ examples/layouts/horizontal container/main.js
@@ -1,1 +1,36 @@
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+const sections = gsap.utils.toArray('.section');
+const scrollContainer = document.querySelector('#pages');
+
+const scrollTween = gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '#pages',
+    pin: true,
+    start: 'top top',
+    scrub: 1,
+    end: () => `+=${scrollContainer.scrollWidth - innerWidth}`,
+  },
+});
+
+const links = document.querySelectorAll('.link');
+for (let link of links) {
+  link.addEventListener('click', onLinkClick);
+}
+
+function onLinkClick(e) {
+  e.preventDefault();
+
+  const target = document.querySelector(e.target.getAttribute('href'));
+  const index = target.dataset.index;
+
+  gsap.to(window, {
+    scrollTo: {
+      y: index * innerWidth,
+      autoKill: false,
+    },
+    duration: 1,
+  });
+}
