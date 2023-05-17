d0db3983b4a4e0d87f8baa45467effb5a0a25a31
Aurimas Cincikas <aurcin@gmail.com>
2023-05-17 14:10:59 +0300

    gsap easing

---
+++ basic methods/easing/main.js
@@ -0,0 +1,127 @@
+gsap.to('.cube[data-cube="10"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'linear',
+});
+
+gsap.to('.cube[data-cube="11"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'linear.in',
+});
+
+gsap.to('.cube[data-cube="12"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'linear.inOut',
+});
+
+gsap.to('.cube[data-cube="13"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'power0',
+});
+
+gsap.to('.cube[data-cube="20"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'bounce',
+});
+
+gsap.to('.cube[data-cube="21"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'bounce.in',
+});
+
+gsap.to('.cube[data-cube="22"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'bounce.inOut',
+});
+
+gsap.to('.cube[data-cube="30"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'elastic',
+});
+
+gsap.to('.cube[data-cube="31"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'elastic.in',
+});
+
+gsap.to('.cube[data-cube="32"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'elastic.inOut',
+});
+
+gsap.to('.cube[data-cube="40"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'back',
+});
+
+gsap.to('.cube[data-cube="41"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'back.in',
+});
+
+gsap.to('.cube[data-cube="42"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'back.inOut',
+});
+
+gsap.to('.cube[data-cube="43"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'back(5)',
+});
+
+gsap.to('.cube[data-cube="44"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'back.in(5)',
+});
+
+gsap.to('.cube[data-cube="45"]', {
+  duration: 3,
+  repeat: -1,
+  repeatDelay: 1,
+  x: 635,
+  ease: 'back.inOut(5)',
+});
