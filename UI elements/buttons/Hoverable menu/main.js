e1942299a5dd10d7182ce0b5aab1af62a7d2dbf3
Aurimas Cincikas <aurcin@gmail.com>
2023-06-05 04:57:52 +0300

    hoverable menu items

---
+++ UI elements/buttons/Hoverable menu/main.js
@@ -0,0 +1,21 @@
+const items = gsap.utils.toArray('.menu-item');
+
+items.forEach(item => {
+  animate(item);
+});
+
+function animate(target) {
+  const timeline = gsap.timeline({ paused: true });
+
+  const link = target.querySelector('.link');
+  const border = target.querySelector('.border');
+
+  gsap.set(border, { scaleX: 0 });
+
+  timeline
+    .to(link, { color: '#6eb5d6', y: -10, duration: 0.2 })
+    .to(border, { backgroundColor: '#6eb5d6', scaleX: 1, duration: 0.5 }, 0);
+
+  target.addEventListener('mouseenter', () => timeline.play());
+  target.addEventListener('mouseleave', () => timeline.timeScale(2).reverse());
+}
